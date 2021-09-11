521e1a90d8a61b8c161c3ac27588dadd5f7e9ea5
asvindra <aswin1399904@gmail.com>
2021-09-11 16:24:19 +0530

    People of GOT block-3

--- block-BJaadq/code/index.js
+++ block-BJaadq/code/index.js
@@ -2,34 +2,77 @@
 
 function countAllPeople() {
   // your code goes here
+ let sum=0;
+ console.log(got.houses.map(p=>{
+   return p.people.length;
+ }).map(total=>{
+   sum=total+sum;
+   return sum;
+ }).pop());
 }
 
 function peopleByHouses() {
   // your code goes here
+  let second= got.houses.map(obj=>{
+    return {[obj.name]:obj.people.length};
+  });
+  return Object.assign({},...second);
+
 }
 
 function everyone() {
   // your code goes here
+  let array=got.houses.map(obj=>{
+    return obj.people.map(name=>{
+      return name.name;
+    })
+  })
+  return array.flat(1);
+
 }
 
 function nameWithS() {
   // your code goes here
+  let nameincludesS=everyone();
+  return nameincludesS.filter(n=>n.includes('s')||n.includes('S'));
+  
 }
 
 function nameWithA() {
   // your code goes here
+  let nameincludesA=everyone();
+  return nameincludesA.filter(n=>n.includes('a')||n.includes('A'));
 }
 
 function surnameWithS() {
   // your code goes here
+  let surNameStartWithS=everyone();
+
+  return surNameStartWithS.filter(n=>{
+    if(n.includes('S',1)){
+      return n;
+    }
+  });
 }
 
 function surnameWithA() {
   // your code goes here
+  let surNameStartWithA=everyone();
+
+  return surNameStartWithA.filter(n=>{
+    if(n.includes('A',1)){
+      return n;
+    }
+  });
 }
 
 function peopleNameOfAllHouses() {
   // your code goes here
+  let second= got.houses.map(obj=>{
+    return {[obj.name]:[obj.people.map(n=>n.name)]};
+  });
+  return Object.assign({},...second);
+
 }
 
 // Testing your result after writing your function
@@ -48,7 +91,7 @@ console.log(nameWithS(), 'with s');
 // Output should be
 // ["Eddard "Ned" Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon "Bran" Stark", "Rickon Stark", "Jon Snow", "Tywin Lannister", "Tyrion Lannister", "Jaime Lannister", "Queen Cersei (Lannister) Baratheon", "Stannis Baratheon", "Daenerys Targaryen", "Viserys Targaryen", "Loras Tyrell", "Catelyn (Tully) Stark", "Lysa (Tully) Arryn"]
 
-console.log(nameWithA());
+console.log(nameWithA(),`with a`);
 // Output should be
 // ["Eddard Stark", "Benjen Stark", "Robb Stark", "Sansa Stark", "Arya Stark", "Brandon Stark", "Rickon Stark", "Tywin Lannister", "Tyrion Lannister", "Jaime Lannister", "Cersei Baratheon", "Robert Baratheon", "Stannis Baratheon", "Renly Baratheon", "Joffrey Baratheon", "Tommen Baratheon", "Myrcella Baratheon", "Daenerys Targaryen", "Viserys Targaryen", "Balon Greyjoy", "Yara Greyjoy", "Margaery Baratheon", "Loras Tyrell", "Catelyn Stark", "Lysa Arryn", "Olenna Tyrell", "Walder Frey", "Jon Arryn", "Khal Drogo"]
 
